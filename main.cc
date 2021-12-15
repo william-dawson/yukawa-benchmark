c7f605c6643e861f9e1dace725ed2fd64d091662
William <william.dawson@riken.jp>
2021-12-15 11:30:35 +0900

    Lint

--- main.cc
+++ main.cc
@@ -1,44 +1,77 @@
+#include <boost/timer/progress_display.hpp>
 #include <libint2.hpp>
 #include <stdexcept>
 #include <vector>
 
-int main(int argc, char* argv[]) {
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::invalid_argument;
+using std::ofstream;
+using std::stod;
+using std::string;
+using std::vector;
+
+int main(int argc, char *argv[]) {
   // Setup
   if (argc < 6) {
-    throw std::invalid_argument("Arguments: xyzfile, basis name");
+    throw invalid_argument("Arguments: xyzfile, basis name, gamma, "
+                           "threshold, output");
   }
-  std::string xyzfile = argv[1];
-  std::string bname = argv[2];
-  double gamma_val = std::stod(argv[3]);
-  double threshold = std::stod(argv[4]);
-  std::string out = argv[5];
+  string xyzfile = argv[1];
+  string bname = argv[2];
+  double gamma_val = stod(argv[3]);
+  double threshold = stod(argv[4]);
+  string out = argv[5];
   libint2::initialize();
 
+  cout << "Parameters:" << endl;
+  cout << "  "
+       << "xyzfile: " << xyzfile << endl;
+  cout << "  "
+       << "basis: " << bname << endl;
+  cout << "  "
+       << "gamma: " << gamma_val << endl;
+  cout << "  "
+       << "threshold: " << threshold << endl;
+  cout << "  "
+       << "output: " << out << endl;
+
   // Geometry and Basis
-  std::ifstream ifile(xyzfile);
+  ifstream ifile(xyzfile);
   auto geom = libint2::read_dotxyz(ifile);
   libint2::BasisSet basis(bname, geom);
 
+  // Compute the number of electrons
+  int nel = 0;
+  for (int i = 0; i < geom.size(); ++i) {
+    nel += geom[i].atomic_number;
+  }
+
   // Create the Engine
-  libint2::Engine eng(libint2::Operator::yukawa, basis.max_nprim(), basis.max_l());
+  libint2::Engine eng(libint2::Operator::yukawa, basis.max_nprim(),
+                      basis.max_l());
   eng.set_params(gamma_val);
 
   // Results stored here
-  std::vector<int> rows, cols;
-  std::vector<double> vals;
+  vector<int> rows, cols;
+  vector<double> vals;
 
   // Compute
-  const auto& buffer = eng.results();
+  const auto &buffer = eng.results();
   auto shell_lookup = basis.shell2bf();
+  boost::timer::progress_display prog(basis.size());
   for (int i = 0; i < basis.size(); ++i) {
     for (int j = 0; j < basis.size(); ++j) {
-      if (i > j) continue;
+      if (i > j)
+        continue;
 
       eng.compute(basis[i], basis[i], basis[j], basis[j]);
       auto data = buffer[0];
 
       // check screening
-      if (data == nullptr) continue;
+      if (data == nullptr)
+        continue;
 
       // unpack
       int offset_i = shell_lookup[i];
@@ -46,24 +79,31 @@ int main(int argc, char* argv[]) {
       int n = basis[i].size();
       int m = basis[j].size();
       for (int k = 0; k < n; ++k) {
-	for (int l = 0; l < m; ++l) {
-	  if (data[k*n + l] > 1e-8) {
-	    vals.push_back(data[k*n + l]);
-	    rows.push_back(offset_i + k);
-	    cols.push_back(offset_j + l);
-	  }
-	}
+        for (int l = 0; l < m; ++l) {
+          if (data[k * n + l] > threshold) {
+            vals.push_back(data[k * n + l]);
+            rows.push_back(offset_i + k);
+            cols.push_back(offset_j + l);
+          }
+        }
       }
     }
+    ++prog;
   }
 
+  // Write out statistics
+  cout << "System Information:" << endl;
+  cout << "  Sparsity: " << double(vals.size()) / (basis.nbf() * basis.nbf()) << endl;
+  cout << "  NBF: " << basis.nbf() << endl;
+  cout << "  Electrons: " << nel << endl;
+
   // Write out to file
-  std::ofstream ofile(out);
-  ofile << "%%MatrixMarket matrix coordinate real symmetric" << std::endl;
-  ofile << "%" << std::endl;
-  ofile << basis.nbf() << " " << basis.nbf() << " " << vals.size() << std::endl;
+  ofstream ofile(out);
+  ofile << "%%MatrixMarket matrix coordinate real symmetric" << endl;
+  ofile << "%" << endl;
+  ofile << basis.nbf() << " " << basis.nbf() << " " << vals.size() << endl;
   for (int i = 0; i < vals.size(); ++i) {
-    ofile << rows[i] + 1 << " " << cols[i] + 1 << " " << vals[i] << std::endl;
+    ofile << rows[i] + 1 << " " << cols[i] + 1 << " " << vals[i] << endl;
   }
   ofile.close();
 
@@ -71,4 +111,3 @@ int main(int argc, char* argv[]) {
   libint2::finalize();
   return 0;
 }
-
